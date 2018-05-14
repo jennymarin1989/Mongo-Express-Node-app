f5ac0e1211aaabcdb575d6a641e79182bfe9e154
jose <jennymarin1989@hotmail.com>
2018-05-14 17:24:36 +0100

    :construction: Add server.js file, add express, mongo and bodyparser modules

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const MongoClient = require('mongodb');
+const bodyParser = require('body-parser');
+
+const app = express() // Express object to handle http methods.
